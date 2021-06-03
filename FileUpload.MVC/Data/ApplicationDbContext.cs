8b82f1ea381bcf1f16f81391d10bdc99dcbcdeb9
Saqib Muhammad Saeed <saqib.saeed.101@outlook.com>
2021-06-03 16:04:24 +0500

    Add project files.

---
+++ FileUpload.MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FileUpload.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<FileOnDatabaseModel> FilesOnDatabase { get; set; }
+        public DbSet<FileOnFileSystemModel> FilesOnFileSystem { get; set; }
+    }
+}
